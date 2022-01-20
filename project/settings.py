0d41c2da10006b21d4c9902e868e1698cea88077
Pachwenko <Pachwenko@users.noreply.github.com>
2022-01-20 03:39:23 +0000

    deploy: faa3b0c9d035cb2afca525f55895360ebfc8f791

--- project/settings.py
+++ project/settings.py
@@ -184,8 +184,6 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
-
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
@@ -201,7 +199,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # whitenoise configuration
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 sentry_sdk.init(
     # automatically grabs the dsn from os.environ['SENTRY_DSN']
