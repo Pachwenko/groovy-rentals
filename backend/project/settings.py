a041ceba787efe19163bc4592e4e8be69f7e5670
Pachwenko <pmccart4096@gmail.com>
2022-01-31 23:55:32 -0600

    clean up static file stuff

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -74,6 +74,7 @@ if DEBUG:
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
