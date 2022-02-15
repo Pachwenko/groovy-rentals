cdc3e8575c2ec291364c3b6505fbfd4e2f930a6a
Patrick McCartney <pmccart4096@gmail.com>
2022-02-14 20:02:14 -0700

    Add Sentry to frontend (#3)
    * enable sourcemaps
    
    * add sentry for frontend error and performance monitoring
    
    * add broken code to test error tracking

--- frontend/config/dotenv.js
+++ frontend/config/dotenv.js
@@ -6,7 +6,7 @@ const path = require('path');
 
 module.exports = function (/* env */) {
   return {
-    clientAllowedKeys: ['MAPBOX_ACCESS_TOKEN'],
+    clientAllowedKeys: ['MAPBOX_ACCESS_TOKEN', 'SENTRY_DSN'],
     fastbootAllowedKeys: [],
     failOnMissingKey: true,
     path: path.join(path.dirname(__dirname), '.env'),
