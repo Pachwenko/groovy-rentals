a03dcb87cf67f01a44e3555980393f47846a4bdb
Patrick McCartney <pmccart4096@gmail.com>
2022-06-11 23:40:51 +0000

    add more localhost options to CORS origins

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -69,6 +69,8 @@ if DEBUG:
     CORS_ALLOWED_ORIGINS += [
         "http://localhost:8000",
         "http://localhost:4200",
+        "http://127.0.0.1:4200",
+        "http://127.0.0.1:8000",
     ]
 
 CSRF_TRUSTED_ORIGINS = CORS_ALLOWED_ORIGINS
